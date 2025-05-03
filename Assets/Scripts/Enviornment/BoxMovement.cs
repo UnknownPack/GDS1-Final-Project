e4448c4adf2543dc4e99457435f73bc69fdd1ca2
Nestor Batoon <batoonnestor@gmail.com>
2025-05-04 03:02:26 +1000

    Update BoxMovement.cs

--- Assets/Scripts/Enviornment/BoxMovement.cs
+++ Assets/Scripts/Enviornment/BoxMovement.cs
@@ -58,6 +58,8 @@ public class BoxMovement : MonoBehaviour
             yield break;
         }
         
+        yield return new WaitForSeconds(waitTime);
+        
         _animator.SetBool("IsDown", true);
         _detector.isSet = true;
         
