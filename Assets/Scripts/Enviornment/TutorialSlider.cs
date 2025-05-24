242c429534fb209e54a814ec76a970dcb180d398
MathgeniusTB2 <daniel.j.martirosov@student.uts.edu.au>
2025-05-24 13:12:11 +1000

    fixed all tutorial levels and added sliders to them

--- Assets/Scripts/Enviornment/TutorialSlider.cs
+++ Assets/Scripts/Enviornment/TutorialSlider.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class TutorialMove : MonoBehaviour
 {
     public GameManager.PostProcessingEffect postProcessingEffect;
-    public float transitionTimeToMax = 0.5f;
-    public float transitionTimeToDefault = 0.5f;
-    public float transitionTimeToMin = 0.5f;
+    public float MaxTime = 0.5f;
+    public float DefaultTime = 0.5f;
+    public float MinTime = 0.5f;
 
     private void Start()
     {
@@ -19,18 +19,18 @@ public class TutorialMove : MonoBehaviour
         {
             // Min to Default
             GameManager.Instance.TransitionExternal(postProcessingEffect, GameManager.Setting.Default, 0.5f);
-            yield return new WaitForSeconds(transitionTimeToDefault);
+            yield return new WaitForSeconds(DefaultTime);
             // Default to Max
             GameManager.Instance.TransitionExternal(postProcessingEffect, GameManager.Setting.Max, 0.5f);
-            yield return new WaitForSeconds(transitionTimeToMax);
+            yield return new WaitForSeconds(MaxTime);
 
             // Max to Default
             GameManager.Instance.TransitionExternal(postProcessingEffect, GameManager.Setting.Default, 0.5f);
-            yield return new WaitForSeconds(transitionTimeToDefault);
+            yield return new WaitForSeconds(DefaultTime);
 
             // Default to Min
             GameManager.Instance.TransitionExternal(postProcessingEffect, GameManager.Setting.Min, 0.5f);
-            yield return new WaitForSeconds(transitionTimeToMin);
+            yield return new WaitForSeconds(MinTime);
         }
     }
 }
